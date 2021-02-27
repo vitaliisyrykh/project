99635a07257d660c1b2d02f214448f7181118cc2
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-02-27 16:55:58 +0200

    add header and browseRouter in compon

--- src/components/form-page/index.jsx
+++ src/components/form-page/index.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
-import Header from './header'
+import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import SignIn from './sign-in-page/form';
+import styles from './form-page.module.scss';
 
-const formPage = props => {
+const FormPage = props => {
+  const imgMainLogo = 'https://www.squadhelp.com/img/logo.png'
+  const styles = {
+    backgroundColor: 'black'
+  }
+  let isSignIn = true;
+  const handleIssignBtn =()=>{
+    isSignIn = !isSignIn;
+  }
+
+
   return (
-    <>
-      <Header />
-      <h2>LOGIN TO YOU ACCOUNT</h2>
-      {/* <Form /> */}
-    </>
-  );
-};
+    <BrowserRouter>
+      <main>
+        <header>
+          <Link to='/'>
+            <img alt='mainLogo' src={imgMainLogo} style={styles} />
+          </Link>
+          <Link to='/signIn'>
+            <button onClick={handleIssignBtn}>{isSignIn?'Sign In':'Sign Up'}</button>
+          </Link>
+        </header>
 
-export default formPage;
+        <Switch>
+          {/* <Route path='/'>
+            <FormPage/>
+          </Route> */}
+          <Route path='/signIn'>
+            <SignIn />
+          </Route>
+          {/* <Route path='/signUp'>
+            <SignUP/>
+          </Route> */}
+        </Switch>
+      </main>
+    </BrowserRouter>
+  )
+}
+
+export default FormPage
