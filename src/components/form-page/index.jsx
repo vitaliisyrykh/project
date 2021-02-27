8a2dc83192fb0746000a79847f1d1c09658f3c82
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-02-27 21:52:14 +0200

    add classComponentForFormPage

--- src/components/form-page/index.jsx
+++ src/components/form-page/index.jsx
@@ -3,41 +3,50 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import SignIn from './sign-in-page/form';
 import styles from './form-page.module.scss';
 
-const FormPage = props => {
-  const imgMainLogo = 'https://www.squadhelp.com/img/logo.png'
-  
-  let isSignIn = true;
-  const handleIssignBtn =()=>{
-    isSignIn = !isSignIn;
+class FormPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isSignIn : false,
+      imgMainLogo : 'https://www.squadhelp.com/img/logo.png',
+    }
   }
-
-
-  return (
-    <BrowserRouter>
-      <main className={styles.main}>
-        <header className={styles.header}>
-          <Link to='/'>
-            <img alt='mainLogo' src={imgMainLogo} style={styles} />
-          </Link>
-          <Link to='/signIn'>
-            <button onClick={handleIssignBtn}>{isSignIn?'Sign In':'Sign Up'}</button>
-          </Link>
-        </header>
-
-        <Switch>
-          {/* <Route path='/'>
-            <FormPage/>
-          </Route> */}
-          <Route path='/signIn'>
-            <SignIn />
-          </Route>
-          {/* <Route path='/signUp'>
-            <SignUP/>
-          </Route> */}
-        </Switch>
-      </main>
-    </BrowserRouter>
-  )
+  
+    handleIssignBtn= (e) => {
+     this.setState ({
+       isSignIn : !this.state.isSignIn,
+      }); 
+  };
+  
+  render(){
+    return (
+      <BrowserRouter>
+        <main className={styles.main}>
+          <header className={styles.header}>
+            <Link to='/'>
+              <img alt='mainLogo' src={this.state.imgMainLogo}  />
+            </Link>
+            <Link to='/signIn' onClick={this.handleIssignBtn}>
+              <button >{this.state.isSignIn?'Sign In':'Sign Up'}</button>
+            </Link>
+  
+          </header>
+          <h1>LOGIN TO YOU ACCOUNT</h1>
+          <Switch>
+            {/* <Route path='/'>
+              <FormPage/>
+            </Route> */}
+            <Route path='/signIn'>
+              <SignIn />
+            </Route>
+            {/* <Route path='/signUp'>
+              <SignUP/>
+            </Route> */}
+          </Switch>
+        </main>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default FormPage
