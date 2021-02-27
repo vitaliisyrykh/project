4b9d819f489ff74e5571b2e02b81db0c3b8ccd19
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-02-27 17:21:12 +0200

    add formik and field for signUp

--- src/components/form-page/sign-up-page/index.jsx
+++ src/components/form-page/sign-up-page/index.jsx
@@ -2,11 +2,42 @@
  import { Formik, Field, Form, ErrorMessage } from 'formik';
 
  const SignUP = props => {
-   return(
+  const onSubmit = (value, formikBag) => {
+    console.log(value)
+    console.log(formikBag)
+  }
+   
+  
+  return(
      <section>
-       <Formik>
-         
+       <Formik validationSchema={SIGN_IN_SCHEMA} onSubmit={onSubmit}>
+       {formProps => {
+        return (
+          <Form>
+            <Field name='name' placeholder='First Name' />
+            <ErrorMessage component='span' name='name' />
+
+            <Field name='name' placeholder='Last Name' />
+            <ErrorMessage component='span' name='name' />
+
+            <Field name='nikName' placeholder='Display Name' />
+            <ErrorMessage component='span' name='nikName' />
+            
+            <Field name='email' placeholder='Email'/>
+            <ErrorMessage name='email' component='span'/>
+
+            <Field name='password' type='password' placeholder='Password'/>
+            <ErrorMessage component='span' name='password' />
+            
+            <Field name='' type='password' placeholder='Confirm Password'/>
+            
+            <Field type='submit' value='LOGIN' />
+          </Form>
+        )
+      }}
        </Formik>
      </section>
    )
  }
+
+ export default SignUP;
