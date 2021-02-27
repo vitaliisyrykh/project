08dbede762fb082967a99d9f9163df9f3d87de9a
vitaliisyrykh <vitaliysyryck@gmail.com>
2021-02-27 16:56:37 +0200

    add fieldAndErrorMessege

--- src/components/form-page/header/index.jsx
+++ src/components/form-page/header/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import SignIn from '../sign-in-page/form'
+
 const Header = props => {
   return (
     <header>
